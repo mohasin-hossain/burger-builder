5f80fe570fbc6c7d75b496cae83af5dca05207af
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2021-02-09 17:55:10 +0600

    Saving orders to Database

--- src/Components/Orders/Checkout/Checkout.js
+++ src/Components/Orders/Checkout/Checkout.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
 import { Button } from "reactstrap";
+import { connect } from "react-redux";
+import axios from 'axios';
+
+const mapStateToProps = (state) => {
+  return {
+    ingredients: state.ingredients,
+    totalPrice: state.totalPrice,
+    purchasable: state.purchasable,
+  };
+};
 
 class Checkout extends Component {
   state = {
@@ -19,23 +29,43 @@ class Checkout extends Component {
       values: {
         ...this.state.values,
         [e.target.name]: e.target.value,
-      }
-    })
-  }
+      },
+    });
+  };
 
   submitHandler = () => {
-    console.log(this.state.values);
-  }
+    const order = {
+      ingredients: this.props.ingredients,
+      customer: this.state.values,
+      price: this.props.totalPrice,
+      orderTime: new Date(),
+    }
+    axios.post("https://burger-builder-65f4e-default-rtdb.firebaseio.com/orders.json", order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
+  };
 
   render() {
     return (
       <div>
-        <form style={{
-          border: "1px solid grey",
-          boxShadow: "1px 1px #888888",
-          borderRadius: "5px",
-          padding: "20px"
-        }}>
+        <h4
+          style={{
+            border: "1px solid grey",
+            boxShadow: "1px 1px #888888",
+            borderRadius: "5px",
+            padding: "20px",
+          }}
+        >
+          Payment: {this.props.totalPrice} BDT
+        </h4>
+        <form
+          style={{
+            border: "1px solid grey",
+            boxShadow: "1px 1px #888888",
+            borderRadius: "5px",
+            padding: "20px",
+          }}
+        >
           <textarea
             name="deliveryAddress"
             value={this.state.values.deliveryAddress}
@@ -66,14 +96,10 @@ class Checkout extends Component {
             style={{ backgroundColor: "#D70F64" }}
             className="mr-auto"
             onClick={this.submitHandler}
-
           >
             Place Order
           </Button>
-          <Button 
-            color="secondary" 
-            className="ml-1" 
-            onClick={this.goBack}>
+          <Button color="secondary" className="ml-1" onClick={this.goBack}>
             Cancel
           </Button>
         </form>
@@ -82,4 +108,4 @@ class Checkout extends Component {
   }
 }
 
-export default Checkout;
+export default connect(mapStateToProps)(Checkout);
