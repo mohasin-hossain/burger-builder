b13682fc453685a708c59a89652f6294540f3bfd
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2021-02-10 15:14:03 +0600

    Completing Orders component

---
+++ src/Components/Orders/Order/Order.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Order = (props) => {
+  const ingredientSummary = props.order.ingredients.map((item) => {
+    return (
+      <span
+        style={{
+          border: "1px solid grey",
+          borderRadius: "5px",
+          marginRight: "10px",
+          padding: "5px",
+        }}
+        key={item.type}
+      >
+        {item.amount}x
+        <span style={{ textTransform: "capitalize" }}> {item.type}</span>
+      </span>
+    );
+  });
+  return (
+    <div
+      style={{
+        border: "1px solid grey",
+        boxShadow: "1px 1px #888888",
+        borderRadius: "5px",
+        marginBottom: "10px",
+        padding: "20px",
+      }}
+    >
+      <p>Order Number: {props.order.id}</p>
+      <p>Delivery Address: {props.order.customer.deliveryAddress}</p>
+      <hr />
+      {ingredientSummary}
+      <hr />
+      <p>Total: {props.order.price} BDT</p>
+    </div>
+  );
+};
+
+export default Order;
--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,39 +1,78 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchOrders } from '../../redux/actionCreators';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchOrders } from "../../redux/actionCreators";
+import Order from "./Order/Order";
+import Spinner from "../Spinner/Spinner";
 
-const mapStateToProps = state => {
-    return {
-        orders: state.orders,
-        orderLoading: state.orderLoading,
-        orderErr: state.orderError,
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    orders: state.orders,
+    orderLoading: state.orderLoading,
+    orderErr: state.orderErr,
+  };
+};
 
-const mapDispatchToProps = dispatch => {
-    return {
-        fetchOrders: () => dispatch(fetchOrders())
-    }
-}
-
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchOrders: () => dispatch(fetchOrders()),
+  };
+};
 
 class Orders extends Component {
-    componentDidMount() {
-        this.props.fetchOrders();
+  componentDidMount() {
+    this.props.fetchOrders();
+  }
+
+  componentDidUpdate() {
+    console.log(this.props);
+  }
+
+  render() {
+    let orders = null;
+    if (this.props.orderErr) {
+      orders = (
+        <p
+          style={{
+            border: "1px solid grey",
+            boxShadow: "1px 1px #888888",
+            borderRadius: "5px",
+            marginBottom: "10px",
+            padding: "20px",
+            backgroundColor: "#D70F64",
+            color: "white",
+          }}
+        >
+          Sorry Failed to Load orders!
+        </p>
+      );
+    } else {
+      if (this.props.orders.length === 0) {
+        orders = (
+          <p
+            style={{
+              border: "1px solid grey",
+              boxShadow: "1px 1px #888888",
+              borderRadius: "5px",
+              marginBottom: "10px",
+              padding: "20px",
+              backgroundColor: "#D70F64",
+              color: "white",
+            }}
+          >
+            You have no Orders!
+          </p>
+        );
+      } else {
+        orders = this.props.orders.map((order) => {
+          return <Order order={order} key={order.id} />;
+        });
+      }
     }
 
-    componentDidUpdate() {
-        console.log(this.props);
-    }
-
-    render() {
-        return (
-            <div>
-                <p>Orders</p>
-            </div>
-        )
-    }
+    return (
+        <div>{this.props.orderLoading ? <Spinner /> : orders}</div>
+    ) 
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Orders);
-
